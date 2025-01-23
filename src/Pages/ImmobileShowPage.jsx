2a3ddc44844e7e8e3ddff1c114b7bd2cf80b8f98
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-01-23 16:08:29 +0100

    fix:pagina dettaglio immobile statica

--- src/Pages/ImmobileShowPage.jsx
+++ src/Pages/ImmobileShowPage.jsx
@@ -1,21 +1,72 @@
 // COMPONENTS IMPORT
 import CardProprietario from "../components/CardProprietario";
 
-
 // COMPONENT EXPORT
 export default function ImmobileShowPage() {
   return (
     <>
       <div className="container">
-        <h1>PAGINA DETTAGLIO IMMOBILE</h1>
-
-        <CardProprietario
-          id="1"
-          email="esempio@esempio.it"
-          telefono="1234567890"
-          nome="Camillo"
-          cognome="Rosanio"
-        />
+        <h1 className="h3 mb-3">Titolo Immobile</h1>
+        <div className="image-container d-lg-flex align-items-center gap-1 rounded-1">
+          <div className="bigger-image-container">
+            <img
+              className="image-fluid rounded-1"
+              src="https://placehold.co/600x400"
+              alt=""
+            />
+          </div>
+          <div className="stacked-image-container d-none d-lg-flex flex-column gap-1 rounded-1">
+            <img
+              className="image-fluid rounded-1"
+              src="https://placehold.co/300x200"
+              alt=""
+            />
+            <img
+              className="image-fluid rounded-1"
+              src="https://placehold.co/300x200"
+              alt=""
+            />
+          </div>
+          <div className="stacked-image-container d-none d-lg-flex flex-column gap-1 rounded-1">
+            <img
+              className="image-fluid rounded-1"
+              src="https://placehold.co/300x200"
+              alt=""
+            />
+            <img
+              className="image-fluid rounded-1"
+              src="https://placehold.co/300x200"
+              alt=""
+            />
+          </div>
+        </div>
+        <h2 className="h4 mt-3">Città, Indirizzo</h2>
+        <div className="row">
+          <div className="col-12 col-md-8">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
+              quaerat, neque consectetur cumque fugit eius pariatur quod nobis
+              ab molestiae aut placeat reiciendis quas rerum modi veritatis
+              culpa totam tempore. Incidunt laboriosam atque, veritatis
+              asperiores modi quos. Consequuntur accusantium unde eius dolorum
+              id esse provident veniam quos tempora, rerum nihil sed assumenda
+              ut dicta facere velit quis! Quidem, repellat doloremque.
+              Asperiores vitae ipsa nesciunt quo similique vel eveniet nam,
+              minima, quod autem nulla officiis blanditiis aut nobis? Nobis
+              ullam nesciunt sapiente debitis ratione, perferendis natus quas
+              fuga voluptate praesentium ipsum?
+            </p>
+          </div>
+          <div className="col-12 col-md-4">
+            <CardProprietario
+              id="1"
+              email="esempio@esempio.it"
+              telefono="1234567890"
+              nome="Camillo"
+              cognome="Rosanio"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
