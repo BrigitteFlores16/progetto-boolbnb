c6c6baf4596c5e89abeb2c7032cab761ea2a8f34
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-01-23 12:26:22 +0100

    add card in HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,6 +6,16 @@ import CardProprietario from "../components/CardProprietario";
 // COMPONENT EXPORT
 
 export default function HomePage() {
+  const immobili = {
+    titolo: "Titolo",
+    num_likes: "Numero like",
+    indirizzo: "Indirizzo",
+    num_letti: "Numero letti",
+    num_bagni: "Numero bagnmi",
+    mq: "Metri quadrati",
+    voto: "voto",
+  };
+
   return (
     <>
       <h1>Home Page</h1>
@@ -17,6 +27,15 @@ export default function HomePage() {
         nome="Camillo"
         cognome="Rosanio"
       />
+
+      <div className="container-card-prova">
+        <ImmobileCard immobile={immobili} />
+        <ImmobileCard immobile={immobili} />
+        <ImmobileCard immobile={immobili} />
+        <ImmobileCard immobile={immobili} />
+        <ImmobileCard immobile={immobili} />
+        <ImmobileCard immobile={immobili} />
+      </div>
     </>
   );
 }
