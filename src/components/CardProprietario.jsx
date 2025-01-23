4cea118bbc3c9c3f414ccedd7c32fd65ccc3c270
MyUsername <rosanio.c@gmail.com>
2025-01-23 11:20:04 +0100

    component - CardProprietario stylig

--- src/components/CardProprietario.jsx
+++ src/components/CardProprietario.jsx
@@ -2,12 +2,12 @@
 
 export default function CardProprietario({ id, email, telefono, nome, cognome }) {
     return <>
-        <div key={id} className='p-2 d-flex flex-column'>
+        <div key={id} className='p-4 d-flex flex-column bg-light rounded-25 gap-3'>
 
             {/* NOME PROFILO */}
-            <div className='d-flex justify-content-center align-items-center'>
+            <div className='d-flex align-items-center gap-3'>
                 {/* ICONA LETTERA */}
-                <div className='d-flex justify-content-center align-items-center'>
+                <div className='d-flex justify-content-center align-items-center bg-primary p-2 rounded-circle text-white font-weight-bold'>
                     {nome.charAt(0).toUpperCase()}
                 </div>
                 {/* NOME */}
@@ -15,8 +15,8 @@ export default function CardProprietario({ id, email, telefono, nome, cognome })
             </div>
 
             {/* CONTATTI */}
-            <p>Telefono: {telefono}</p>
-            <p>Email: {email}</p>
+            <p>Telefono: <span className="text-primary">{telefono}</span></p>
+            <p>Email: <span className="text-primary">{email}</span></p>
 
         </div>
     </>
