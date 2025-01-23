fe9a17fbd05023db6c7a6569ae45a6114337b15a
MyUsername <rosanio.c@gmail.com>
2025-01-23 12:44:22 +0100

    Applicazioen classe Debug (CardProprietario)

--- src/components/CardProprietario.jsx
+++ src/components/CardProprietario.jsx
@@ -2,21 +2,21 @@
 
 export default function CardProprietario({ id, email, telefono, nome, cognome }) {
     return <>
-        <div key={id} className='p-4 d-flex flex-column bg-light rounded-25 gap-3 w-50'>
+        <div key={id} className='debug'>
 
             {/* NOME PROFILO */}
-            <div className='d-flex align-items-center gap-3'>
+            <div className='debug'>
                 {/* ICONA LETTERA */}
-                <div className='d-flex justify-content-center align-items-center bg-primary p-2 rounded-circle text-white font-weight-bold'>
-                    {nome.charAt(0).toUpperCase()}
+                <div className='debug'>
+                    icona lettera {nome.charAt(0).toUpperCase()}
                 </div>
                 {/* NOME */}
-                <h3>{nome} {cognome}</h3>
+                <h3 className="debug">{nome} {cognome}</h3>
             </div>
 
             {/* CONTATTI */}
-            <p>Telefono: <span className="text-primary">{telefono}</span></p>
-            <p>Email: <span className="text-primary">{email}</span></p>
+            <p className="debug">Telefono: <span className="debug">{telefono}</span></p>
+            <p className="debug">Email: <span className="debug">{email}</span></p>
 
         </div>
     </>
