ebb565383d7cef39074a6e6ddb2f8a538b238864
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-01-24 09:49:14 +0100

    Add toFixed in ImmobileCard - voto

--- src/Components/ImmobileCard.jsx
+++ src/Components/ImmobileCard.jsx
@@ -28,7 +28,8 @@ export default function ImmobileCard({ immobile }) {
             <i class="fa-solid fa-ruler"></i> {immobile.mq}
           </span>
           <span className="immobile-content-star">
-            <i class="fa-solid fa-star"></i> {parseFloat(immobile.voto)}
+            <i class="fa-solid fa-star"></i>{" "}
+            {parseFloat(immobile.voto).toFixed(1)}
           </span>
         </div>
       </div>
