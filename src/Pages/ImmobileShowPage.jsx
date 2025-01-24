25d1ab994dee030aa3eafac71a9a74f06cf61f84
Alessandro-Fiora <fiora.ale97@gmail.com>
2025-01-24 12:39:32 +0100

    fix: fixed average vote validation on review section

--- src/Pages/ImmobileShowPage.jsx
+++ src/Pages/ImmobileShowPage.jsx
@@ -1,7 +1,6 @@
 // UTILITY
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router';
-
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
 // COMPONENTS IMPORT
 import CardProprietario from "../Components/CardProprietario";
@@ -9,10 +8,8 @@ import ComfortsImmobile from "../Components/ComfortsImmobile";
 import CardRecensione, { getAverageRating } from "../Components/CardRecensione";
 import recensioniArray from "../array_recensioni";
 
-
 // COMPONENT EXPORT
 export default function ImmobileShowPage() {
-
   // GET URL PARAMS
   const { id: id } = useParams();
 
@@ -27,27 +24,28 @@ export default function ImmobileShowPage() {
 
   // AJAX REQUEST - SHOW
   const ajaxShow = () => {
-    fetch('http://localhost:3000/api/immobili/' + id, {
-      method: 'GET',
+    fetch("http://localhost:3000/api/immobili/" + id, {
+      method: "GET",
     })
-      .then(res => res.json())
+      .then((res) => res.json())
       .then((data) => {
         setImmobile(data);
-        console.log('AJAX SHOW request: at http://localhost:3000/' + id);
+        console.log("AJAX SHOW request: at http://localhost:3000/" + id);
         console.log(data);
       })
       .catch((error) => {
-        console.log('Error while fetching content')
-      })
+        console.log("Error while fetching content");
+      });
   };
 
-
-  const averageRating = immobile.recensioni ? getAverageRating(immobile.recensioni) : '';
+  const averageRating = immobile.recensioni
+    ? getAverageRating(immobile.recensioni)
+    : "";
 
   let voteTitle;
   if (averageRating > 4) {
     voteTitle = "Amato dagli ospiti";
-  } else if (averageRating >= 2) {
+  } else if (averageRating >= 3) {
     voteTitle = "Apprezzato dagli ospiti";
   } else {
     voteTitle = "Accettabile";
@@ -62,7 +60,7 @@ export default function ImmobileShowPage() {
             <div className="image-container d-flex justify-content-center">
               <img
                 className="image-fluid h-100 w-100 rounded-1"
-                src={immobile.immagine ? immobile.immagine : ''}
+                src={immobile.immagine ? immobile.immagine : ""}
                 alt=""
               />
             </div>
@@ -79,7 +77,9 @@ export default function ImmobileShowPage() {
           </div>
         </div>
 
-        <h2 className="h5 mt-5">{immobile.città}, {immobile.indirizzo}</h2>
+        <h2 className="h5 mt-5">
+          {immobile.città}, {immobile.indirizzo}
+        </h2>
         <div className="row">
           <div className="col-12 col-md-7">
             <p>
@@ -97,7 +97,7 @@ export default function ImmobileShowPage() {
             </p>
           </div>
           <div className="col-12 col-md-5">
-            {immobile.id_proprietario ?
+            {immobile.id_proprietario ? (
               <CardProprietario
                 id={immobile.id_proprietario}
                 email={immobile.email_proprietario}
@@ -105,22 +105,29 @@ export default function ImmobileShowPage() {
                 nome={immobile.nome_proprietario}
                 cognome={immobile.cognome_proprietario}
               />
-              : ''}
+            ) : (
+              ""
+            )}
           </div>
         </div>
+        <hr />
+
         <div className="my-4"></div>
         <div className="text-center">
-          <h2 className="display-1 fw-bold">{immobile.recensioni ? averageRating.toFixed(1) : ''}</h2>
+          <h2 className="display-1 fw-bold">
+            {immobile.recensioni?.length > 0 ? averageRating.toFixed(1) : "0"}
+          </h2>
           <p className="mb-4">{voteTitle}</p>
           <hr />
         </div>
         <div className="row">
-          {immobile.recensioni ?
-            immobile.recensioni.map((recensione, index) => (
-              <div key={index} className="col-md-6 mb-4">
-                <CardRecensione recensione={recensione} />
-              </div>))
-            : ''}
+          {immobile.recensioni
+            ? immobile.recensioni.map((recensione, index) => (
+                <div key={index} className="col-md-6 mb-4">
+                  <CardRecensione recensione={recensione} />
+                </div>
+              ))
+            : ""}
         </div>
       </div>
     </>
