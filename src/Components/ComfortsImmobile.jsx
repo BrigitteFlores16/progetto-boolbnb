0532f21e73fdf80f2be53e1a5e3941b52cf22e78
MyUsername <rosanio.c@gmail.com>
2025-01-24 10:43:53 +0100

    Show Immobile - info dinamiche

--- src/Components/ComfortsImmobile.jsx
+++ src/Components/ComfortsImmobile.jsx
@@ -14,15 +14,15 @@ export default function ComfortsImmobile({ id, tipologia, num_stanze, num_letti,
                 <h3 className="cardComforts titoloLista">Comforts</h3>
                 <div className="cardComforts listaComforts">
                     {/* TIPOLOGIA */}
-                    {tipologia ? <p className="cardComforts comfort"><i class="fa-solid fa-house cardComforts icona"></i> {tipologia}</p> : ''}
+                    {tipologia ? <p className="cardComforts comfort"><i className="fa-solid fa-house cardComforts icona"></i> {tipologia}</p> : ''}
                     {/* STANZE */}
-                    {num_stanze ? <p className="cardComforts comfort"><i class="fa-solid fa-door-open cardComforts icona"></i> Stanze: {num_stanze}</p> : ''}
+                    {num_stanze ? <p className="cardComforts comfort"><i className="fa-solid fa-door-open cardComforts icona"></i> Stanze: {num_stanze}</p> : ''}
                     {/* LETTI */}
-                    {num_letti ? <p className="cardComforts comfort"><i class="fa-solid fa-bed cardComforts icona"></i> Posti letto: {num_letti}</p> : ''}
+                    {num_letti ? <p className="cardComforts comfort"><i className="fa-solid fa-bed cardComforts icona"></i> Posti letto: {num_letti}</p> : ''}
                     {/* BAGNI */}
-                    {num_bagni ? <p className="cardComforts comfort"><i class="fa-solid fa-shower cardComforts icona"></i> Bagni: {num_bagni}</p> : ''}
+                    {num_bagni ? <p className="cardComforts comfort"><i className="fa-solid fa-shower cardComforts icona"></i> Bagni: {num_bagni}</p> : ''}
                     {/* MQ */}
-                    {mq ? <p className="cardComforts comfort"><i class="fa-regular fa-square cardComforts icona"></i> Metri quadri: {mq}</p> : ''}
+                    {mq ? <p className="cardComforts comfort"><i className="fa-regular fa-square cardComforts icona"></i> Metri quadri: {mq}</p> : ''}
                 </div>
             </div>
         }
--- src/Pages/ImmobileShowPage.jsx
+++ src/Pages/ImmobileShowPage.jsx
@@ -1,51 +1,86 @@
+// UTILITY
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+
+
 // COMPONENTS IMPORT
 import CardProprietario from "../Components/CardProprietario";
 import ComfortsImmobile from "../Components/ComfortsImmobile";
 import CardRecensione, { getAverageRating } from "../Components/CardRecensione";
 import recensioniArray from "../array_recensioni";
 
+
 // COMPONENT EXPORT
 export default function ImmobileShowPage() {
-  const averageRating = getAverageRating(recensioniArray);
 
-  let title;
+  // GET URL PARAMS
+  const { id: id } = useParams();
+
+  // USE-STATE DATA
+  const [immobile, setImmobile] = useState({ immobile: [] });
+
+  // INIT USE-EFFECT
+  useEffect(() => {
+    ajaxShow();
+  }, []);
+
+  // AJAX REQUEST - SHOW
+  const ajaxShow = () => {
+    fetch('http://localhost:3000/api/immobili/' + id, {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then((data) => {
+        setImmobile(data);
+        console.log('AJAX SHOW request: at http://localhost:3000/' + id);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log('Error while fetching content')
+      })
+  };
+
+
+  const averageRating = immobile.recensioni ? getAverageRating(immobile.recensioni) : '';
+
+  let voteTitle;
   if (averageRating > 4) {
-    title = "Amato dagli ospiti";
+    voteTitle = "Amato dagli ospiti";
   } else if (averageRating >= 2) {
-    title = "Apprezzato dagli ospiti";
+    voteTitle = "Apprezzato dagli ospiti";
   } else {
-    title = "Da migliorare";
+    voteTitle = "Accettabile";
   }
 
   return (
     <>
       <div className="container">
-        <h1 className="h3 mb-3">Titolo Immobile</h1>
+        <h1 className="h3 mb-3">{immobile.titolo}</h1>
         <div className="row">
           <div className="col-12 col-md-6">
             <div className="image-container d-flex justify-content-center">
               <img
                 className="image-fluid h-100 w-100 rounded-1"
-                src="https://placehold.co/600x400"
+                src={immobile.immagine ? immobile.immagine : ''}
                 alt=""
               />
             </div>
           </div>
           <div className="col-12 col-md-6">
             <ComfortsImmobile
-              id="1"
-              tipologia="Villetta"
-              num_stanze="5"
-              num_letti="5"
-              num_bagni="3"
-              mq="120"
+              id={id}
+              tipologia={immobile.tipologia}
+              num_stanze={immobile.num_stanze}
+              num_letti={immobile.num_letti}
+              num_bagni={immobile.num_bagni}
+              mq={immobile.mq}
             />
           </div>
         </div>
 
-        <h2 className="h5 mt-5">Città, Indirizzo</h2>
+        <h2 className="h5 mt-5">{immobile.città}, {immobile.indirizzo}</h2>
         <div className="row">
-          <div className="col-12 col-md-8">
+          <div className="col-12 col-md-7">
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
               quaerat, neque consectetur cumque fugit eius pariatur quod nobis
@@ -60,28 +95,31 @@ export default function ImmobileShowPage() {
               fuga voluptate praesentium ipsum?
             </p>
           </div>
-          <div className="col-12 col-md-4">
-            <CardProprietario
-              id="1"
-              email="esempio@esempio.it"
-              telefono="1234567890"
-              nome="Camillo"
-              cognome="Rosanio"
-            />
+          <div className="col-12 col-md-5">
+            {immobile.id_proprietario ?
+              <CardProprietario
+                id={immobile.id_proprietario}
+                email={immobile.email_proprietario}
+                telefono={immobile.telefono_proprietario}
+                nome={immobile.nome_proprietario}
+                cognome={immobile.cognome_proprietario}
+              />
+              : ''}
           </div>
         </div>
         <div className="my-4"></div>
         <div className="text-center">
-          <h2 className="display-1 fw-bold">{averageRating.toFixed(1)}</h2>
-          <p className="mb-4">{title}</p>
+          <h2 className="display-1 fw-bold">{immobile.recensioni ? averageRating.toFixed(1) : ''}</h2>
+          <p className="mb-4">{voteTitle}</p>
           <hr />
         </div>
         <div className="row">
-          {recensioniArray.map((recensione, index) => (
-            <div key={index} className="col-md-6 mb-4">
-              <CardRecensione recensione={recensione} />
-            </div>
-          ))}
+          {immobile.recensioni ?
+            immobile.recensioni.map((recensione, index) => (
+              <div key={index} className="col-md-6 mb-4">
+                <CardRecensione recensione={recensione} />
+              </div>))
+            : ''}
         </div>
       </div>
     </>
