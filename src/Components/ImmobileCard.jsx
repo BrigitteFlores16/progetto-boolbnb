51708b0c8d2bb2a20f892f46f546836aac5b94ee
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-01-28 12:44:33 +0100

    ImmobileCard.jsx

--- src/Components/ImmobileCard.jsx
+++ src/Components/ImmobileCard.jsx
@@ -30,43 +30,47 @@ export default function ImmobileCard({ immobile, refreshData }) {
       />
 
       <Link
-        key={immobile.id}
-        to={`/${immobile.id}`}
+        key={immobile && immobile.id}
+        to={`/${immobile && immobile.id}`}
         className="immobile-card-container"
       >
         {/* header */}
         <div className="img-immobile-container">
-          <img className="img-immobile" src={immobile.immagine} alt="" />
+          <img
+            className="img-immobile"
+            src={immobile && immobile.immagine}
+            alt=""
+          />
         </div>
 
         {/* body */}
         <div className="body-container">
-          <h2>{immobile.titolo}</h2>
+          <h2>{immobile && immobile.titolo}</h2>
 
-          <div className="immobile-card-body-content cardComforts icona">
+          <div className="immobile-card-body-content">
             <span className="immobile-content-like">
-              <i className="fa-solid fa-heart"></i> {immobile.num_likes}
-            </span>
-            <span className="immobile-content-adress cardComforts icona">
-              <i className="fa-solid fa-map-pin"></i> {immobile.indirizzo}
+              <i className="fa-solid fa-heart"></i>{" "}
+              {immobile && immobile.num_likes}
+            </span>{" "}
+            <br />
+            <span className="immobile-content-adress">
+              <i className="fa-solid fa-map-pin"></i>{" "}
+              {immobile && immobile.indirizzo}
             </span>
             <span className="immobile-content-room">
-              <i className="fa-solid fa-door-open"></i> Stanze:{" "}
-              {immobile.num_stanze}
-            </span>
-            <span className="immobile-content-bed">
-              <i className="fa-solid fa-bed"></i> Posti letto:{" "}
-              {immobile.num_letti}
+              <i className="fa-solid fa-user"></i>{" "}
+              {immobile && immobile.num_letti}
             </span>
             <span className="immobile-content-bathroom">
-              <i className="fa-solid fa-shower"></i> Bagni: {immobile.num_bagni}
+              <i className="fa-solid fa-shower"></i>{" "}
+              {immobile && immobile.num_bagni}
             </span>
             <span className="immobile-content-meters">
-              <i className="fa-solid fa-ruler"></i> Metri quadri: {immobile.mq}
+              <i className="fa-solid fa-ruler"></i> {immobile && immobile.mq}
             </span>
-            <span className="immobile-content-star cardComforts icona">
+            <span className="immobile-content-star">
               <i className="fa-solid fa-star"></i>
-              {parseFloat(immobile.voto).toFixed(1)}
+              {parseFloat(immobile && immobile.voto).toFixed(1)}
             </span>
           </div>
         </div>
