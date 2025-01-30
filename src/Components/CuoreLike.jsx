a8143db505476b2e6ee3d72ce533f536f81e5aa2
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-01-30 12:57:48 +0100

    fix CuoreCard layout

--- src/Components/CuoreLike.jsx
+++ src/Components/CuoreLike.jsx
@@ -1,4 +1,9 @@
-export default function CuoreLike({ functionLike, colorHeart, isAbsolute }) {
+export default function CuoreLike({
+  functionLike,
+  colorHeart,
+  isAbsolute,
+  numLike,
+}) {
   console.log(colorHeart);
   return (
     <div
@@ -9,7 +14,15 @@ export default function CuoreLike({ functionLike, colorHeart, isAbsolute }) {
           : "cuore-like-container"
       }
     >
-      <i className={`fa-solid fa-heart immobile-img-heart ${colorHeart}`}></i>
+      {/* <span className="immobile-content-like immobile-img-heart"> */}
+      <div className="heart-container">
+        <span className="num-like-container">{numLike && numLike}</span>
+        <i className={`fa-solid fa-heart immobile-img-heart ${colorHeart}`}></i>
+      </div>
+      {/* </span> */}
     </div>
   );
+}
+{
+  /* <i className="fa-solid fa-heart cardComforts icona"></i>{" "} */
 }
