3ce770d9698acb697a0e5f4db65f6641fd65d7f3
MyUsername <rosanio.c@gmail.com>
2025-01-30 11:12:17 +0100

    filterCity resta attivo con "Cancella Filtri"

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -65,7 +65,7 @@ export default function Searchbar({
     setFilterBeds("");
     setFilterType("");
     console.log(filterCity, filterRooms, filterBeds, filterType);
-    fetchImmobili("", "", "", "");
+    fetchImmobili(filterCity, "", "", "");
   };
 
   //FETCH TIPOLOGIE IMMOBILE
