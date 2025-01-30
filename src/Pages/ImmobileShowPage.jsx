64e0426ea368c679ff28a1f0c96d55018ffac83e
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2025-01-30 12:08:18 +0100

    aggiustato form recensioni design

--- src/Pages/ImmobileShowPage.jsx
+++ src/Pages/ImmobileShowPage.jsx
@@ -201,78 +201,80 @@ export default function ImmobileShowPage() {
         <div className="formStore">
           <h2 className="h3 mb-3">Aggiungi una recensione</h2>
           <form onSubmit={handleReviewSubmit}>
-            <div className="mb-3">
-              <label htmlFor="nomeInput" className="form-label">
-                Nome
-              </label>
-              <input
-                value={newReview.nome}
-                onChange={handleReviewChange}
-                type="text"
-                className="form-control"
-                id="nomeInput"
-                name="nome"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="titoloInput" className="form-label">
-                Titolo
-              </label>
-              <input
-                value={newReview.titolo}
-                onChange={handleReviewChange}
-                type="text"
-                className="form-control"
-                id="titoloInput"
-                name="titolo"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="testoInput" className="form-label">
-                Testo
-              </label>
-              <textarea
-                value={newReview.testo}
-                onChange={handleReviewChange}
-                className="form-control"
-                id="testoInput"
-                name="testo"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="votoInput" className="form-label">
-                Voto
-              </label>
-              <input
-                value={newReview.voto}
-                onChange={handleReviewChange}
-                type="number"
-                className="form-control"
-                id="votoInput"
-                name="voto"
-                min="1"
-                max="5"
-                required
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="numGiorniInput" className="form-label">
-                Numero di giorni di permanenza
-              </label>
-              <input
-                value={newReview.num_giorni_di_permanenza}
-                onChange={handleReviewChange}
-                type="number"
-                className="form-control"
-                id="numGiorniInput"
-                name="num_giorni_di_permanenza"
-                min="1"
-                max="255"
-                required
-              />
+            <div className="row">
+              <div className="col-12 mb-3">
+                <label htmlFor="titoloInput" className="form-label">
+                  Titolo
+                </label>
+                <input
+                  value={newReview.titolo}
+                  onChange={handleReviewChange}
+                  type="text"
+                  className="form-control"
+                  id="titoloInput"
+                  name="titolo"
+                  required
+                />
+              </div>
+              <div className="col-4 mb-3">
+                <label htmlFor="votoInput" className="form-label">
+                  Voto
+                </label>
+                <input
+                  value={newReview.voto}
+                  onChange={handleReviewChange}
+                  type="number"
+                  className="form-control"
+                  id="votoInput"
+                  name="voto"
+                  min="1"
+                  max="5"
+                  required
+                />
+              </div>
+              <div className="col-4 mb-3">
+                <label htmlFor="nomeInput" className="form-label">
+                  Nome
+                </label>
+                <input
+                  value={newReview.nome}
+                  onChange={handleReviewChange}
+                  type="text"
+                  className="form-control"
+                  id="nomeInput"
+                  name="nome"
+                  required
+                />
+              </div>
+              <div className="col-4 mb-3">
+                <label htmlFor="numGiorniInput" className="form-label">
+                  Numero di giorni di permanenza
+                </label>
+                <input
+                  value={newReview.num_giorni_di_permanenza}
+                  onChange={handleReviewChange}
+                  type="number"
+                  className="form-control"
+                  id="numGiorniInput"
+                  name="num_giorni_di_permanenza"
+                  min="1"
+                  max="255"
+                  required
+                />
+              </div>
+              <div className="col-12 mb-3">
+                <label htmlFor="testoInput" className="form-label">
+                  Testo
+                </label>
+                <textarea
+                  value={newReview.testo}
+                  onChange={handleReviewChange}
+                  className="form-control"
+                  id="testoInput"
+                  name="testo"
+                  required
+                />
+              </div>
             </div>
             <button type="submit" className="btn submitNewImmobileButton my-4">
               Invia Recensione
